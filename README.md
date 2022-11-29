20aeb27ec37df6ae395eef2fef042ff9240f5113
stu <stumcg@student.ubc.ca>
2022-11-29 12:21:16 -0800

    Got mapbox running

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # COSC-341-Term-Project
+
+To build create gradle.properties file in your home directory:
+
+Windows:
+```
+C:\Users\<username>\.gradle\gradle.properties
+```
+
+Mac
+```
+/Users/<username/.gradle/gradle.properties
+```
+
+Add the line:
+```
+MAPBOX_DOWNLOADS_TOKEN=<private mapbox token>
+```
+
+(ask stu for token)
--- app/src/main/java/com/example/afrito/MainActivity.java
+++ app/src/main/java/com/example/afrito/MainActivity.java
@@ -4,11 +4,42 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.mapbox.maps.MapView;
+import com.mapbox.maps.Style;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MapView mapView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mapView = (MapView) findViewById(R.id.mapView);
+        mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
     }
 }
